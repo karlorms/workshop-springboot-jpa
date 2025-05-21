435a9716fc01758f677b2a779fc19749854d55f2
Roger Silva <karlorms@gmail.com>
2025-05-21 17:35:10 -0300

    Project created

---
+++ src/test/java/com/roger/workshopspringbootjpa/WorkshopSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roger.workshopspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
