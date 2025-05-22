cda9ec27eeb57b4980a5ce1eda2e9aa3853fe586
Roger Silva <karlorms@gmail.com>
2025-05-22 11:02:16 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/roger/workshopspringbootjpa/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.roger.workshopspringbootjpa.repositories;
+
+import com.roger.workshopspringbootjpa.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
